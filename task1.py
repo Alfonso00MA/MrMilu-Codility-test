0872c2a532d15c3ab185525b461e23386e0d4541
Alfonso <alfonso@alfonso.com>
2024-05-25 04:23:03 +0200

    Improving task description

--- task1.py
+++ task1.py
@@ -1,8 +1,8 @@
 def solution(S):
-    """
-    Given a string S, this function returns the maximum number of moves
-    that can be applied to S, where each move involves deleting the letters
-    in the word "BANANA" from S.
+    """    
+    A string S made of uppercase Eglish letters is given. 
+    In one move, six letters forming the word BANANA can be deleted from S.
+    What is the maximum number of times such a move can be applied to S?
     """
     
     # Counting occurrences for each of the 3 letters
@@ -18,5 +18,15 @@ def solution(S):
         elif char == 'N':
             n_count += 1
     
+    print("B: {} A: {} N: {}".format(b_count, a_count, n_count))
     # Calculate the maximum number of moves
     return min(b_count, n_count//2, a_count//3)
+    
+    
+    
+def test_solution():
+    assert(solution("NAABXXAN")==1)
+    assert(solution("NAANAAXNABABYNNBZ")==2)
+    assert(solution("QABAAAWOBL")==0)
+
+test_solution()
